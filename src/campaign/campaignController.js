0cc201a012242cf1534030c6a6315bf4eb3f34c0
Alvin Varghese <alvin19official@gmail.com>
2024-03-22 18:24:23 +0530

    feat: complete campaign module get requests

--- src/campaign/campaignController.js
+++ src/campaign/campaignController.js
@@ -1,24 +1,98 @@
+const { CustomErrorResponse } = require("../shared/error/errorResponse");
+const SuccessResponse = require("../shared/success/successResponse");
+const causesList = require("../campaign/campaignModel");
+
 const getAllCampaigns = async (req, res) => {
-  res.status(200).json({ message: "getAllCampaigns" });
+  try {
+    const campaigns = await campaignModel.find();
+    if (!campaigns || campaigns.length <= 0)
+      return new CustomErrorResponse(
+        res,
+        "No campaigns present. Create one!",
+        StatusCodes.BAD_REQUEST
+      );
+
+    return new SuccessResponse(
+      res,
+      "Campaigns fetched successfully!",
+      campaigns
+    );
+  } catch (err) {
+    console.error(err.message, err.status);
+  }
 };
+
 const getCausesList = async (req, res) => {
-  res.status(200).json({ message: "getCausesList" });
+  return new SuccessResponse(res, "Campaign causes fetched!", causesList);
 };
+
 const createCampaign = async (req, res) => {
   res.status(200).json({ message: "createCampaign" });
 };
+
 const getUserCampaigns = async (req, res) => {
-  res.status(200).json({ message: "getUserCampaigns" });
+  try {
+    const { id } = req.user;
+    if (!mongoose.isValidObjectId(id))
+      return new CustomErrorResponse(
+        res,
+        "Invalid user ID!",
+        StatusCodes.BAD_REQUEST
+      );
+
+    const campaigns = await campaignModel.find({ creator: id });
+    if (!campaigns || campaigns.length <= 0)
+      return new CustomErrorResponse(
+        res,
+        "The campaign you requested does not exist!",
+        StatusCodes.BAD_REQUEST
+      );
+
+    return new SuccessResponse(
+      res,
+      "Campaigns fetched successfully!",
+      campaigns
+    );
+  } catch (err) {
+    console.error(err.message, err.status || StatusCodes.INTERNAL_SERVER_ERROR);
+  }
 };
+
 const getCampaignById = async (req, res) => {
-  res.status(200).json({ message: "getCampaignById" });
+  try {
+    const { id } = req.params;
+    if (!id)
+      return new BadRequestErrorResponse(res, "Campaign id not present!");
+
+    if (!mongoose.isValidObjectId(id))
+      return new CustomErrorResponse(
+        res,
+        "Invalid campaign ID",
+        StatusCodes.BAD_REQUEST
+      );
+
+    const data = await campaignModel.findById(id);
+    if (!data)
+      return new CustomErrorResponse(
+        res,
+        "The campaign you requested does not exist!",
+        StatusCodes.BAD_REQUEST
+      );
+
+    return new SuccessResponse(res, "Action completed successfully!", data);
+  } catch (err) {
+    console.error(err.message, err.status || StatusCodes.INTERNAL_SERVER_ERROR);
+  }
 };
+
 const updateCampaign = async (req, res) => {
   res.status(200).json({ message: "updateCampaign" });
 };
+
 const deleteCampaign = async (req, res) => {
   res.status(200).json({ message: "deleteCampaign" });
 };
+
 const makeDonation = async (req, res) => {
   res.status(200).json({ message: "makeDonation" });
 };
---
+++ src/campaign/campaignModel.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const causesList = [
+  "Medical",
+  "Artistic",
+  "Humanitarian",
+  "Environmental",
+  "Mental Health",
+  "Disaster Relief",
+  "Entrepreneurship",
+  "Sports Development",
+  "Research & Education",
+  "Other",
+];
+
+const campaignSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  story: {
+    type: [String],
+    required: true,
+  },
+  goal: {
+    type: Number,
+    required: true,
+  },
+  endDate: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  causeType: {
+    type: String,
+    enum: causesList,
+    default: "Other",
+    required: true,
+  },
+});
+
+const campaignModel = mongoose.model("Campaign", campaignSchema);
+
+module.exports = {
+  campaignModel,
+  causesList,
+};
